a9c18d467c42bd347f472d329fd61a0e685441ad
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-13 17:36:32 -0700

    lookin good

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ function shellCommand(command) {
   }
 }
 
-var client = require('scp2');
+var progress = require('progress-stream');
 var fs = require('fs');
 var _ = require('lodash');
 var path = require('path');
@@ -19,31 +19,55 @@ var connectOptions = {
 };
 
 var deployHome = '/home/deploy';
+var userScript = 'ls -lah / && sleep 2 && echo "ok im done" && sleep 1 && echo "just kidding" && ls';
 
 module.exports = {
   init: function (config, job, context, done) {
     done(null, {
       deploy: function(context, done) {
         context.comment("Preparing to deploy...");
-        /*
         var Connection = require('ssh2');
-
         var conn = new Connection();
         conn.on('ready', function() {
           context.comment('Connection :: ready');
-          conn.exec('uptime', function(err, stream) {
-            if (err) throw err;
-            stream.on('end', function() {
-              context.comment('Stream :: EOF');
-            }).on('exit', function(code, signal) {
-              context.comment('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-            }).on('close', function() {
-              context.comment('Stream :: close');
-              conn.end();
-            }).on('data', function(data) {
-              context.comment('STDOUT: ' + data);
-            //}).stderr.on('data', function(data) {
-            //  context.comment('STDERR: ' + data);
+          var name = context.job.project.name.replace('/', '_');
+          context.comment("Bundling project...");
+          var bundlePath = '/tmp/'+name+'.tar.gz';
+          var bundleStream = require('npmd-pack')(context.dataDir, {})
+          .pipe(fs.createWriteStream(bundlePath))
+          .on('finish', function () {
+            context.comment("Created bundle "+bundlePath);
+            var remoteBundlePath = deployHome+'/'+name+'.tar.gz';
+            context.comment('Transferring bundle to '+remoteBundlePath);
+            conn.sftp(function (err, sftp) {
+              if (err) throw err;
+              var writeStream = sftp.createWriteStream(remoteBundlePath);
+              var str = progress({time:500, length: fs.statSync(bundlePath).size});
+              str.on('progress', function (info) { context.comment(info.percentage+"%") });
+              fs.createReadStream(bundlePath).pipe(str)
+              .pipe(writeStream).on('close', function () {
+                var pkgPath = deployHome+'/package';
+                context.comment("Replacing "+pkgPath+" with bundle contents");
+                function devnull(str) { return str+' > /dev/null 2>&1' };
+                conn.exec([
+                  devnull('rm -rf '+deployHome+'/package.previous'),
+                  devnull('mv '+deployHome+'/package '+deployHome+'/package.previous'),
+                  devnull('tar -zxf '+remoteBundlePath),
+                  userScript
+                ].join('\n'), function(err, stream) {
+                  if (err) throw err;
+                  stream.on('end', function() {
+                    context.comment('Stream :: EOF');
+                  }).on('exit', function(code, signal) {
+                    context.comment('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+                  }).on('close', function() {
+                    context.comment('Stream :: close');
+                    conn.end();
+                  }).on('data', function(data) {
+                    context.out(data);
+                  });
+                });
+              });
             });
           });
         }).on('error', function(err) {
@@ -52,42 +76,8 @@ module.exports = {
           context.comment('Connection :: end');
         }).on('close', function(had_error) {
           context.comment('Connection :: close');
-          done();
+          done((had_error ? -1 : 0));
         }).connect(connectOptions);
-       */
-
-
-      /*
-        var client = require('scp2');
-        context.comment('Transferring project to '+remoteDir);
-        client.scp(context.dataDir, _.extend({
-          path: remoteDir
-        }, connectOptions), function(err) {
-          if (err) 
-            context.comment(err);
-          context.comment("Project deployed at "+remoteDir);
-          done();
-        });
-       */
-        var name = context.job.project.name.replace('/', '_');
-
-        var bundlePath = '/tmp/'+name+'.tar.gz';
-        require('npmd-pack')(context.dataDir, {})
-        .pipe(fs.createWriteStream(bundlePath))
-        .on('finish', function () {
-          process.nextTick(function () {
-            context.comment("Created bundle "+bundlePath);
-            var remoteBundlePath = deployHome+'/'+name+'.tar.gz';
-            context.comment('Securely copying bundle to '+remoteBundlePath);
-            client.scp(bundlePath, _.extend({
-              path: remoteBundlePath
-            }, connectOptions), function(err) {
-              if (err) { context.comment(err); return done(-1) }
-              context.comment("Bundle copied to "+remoteBundlePath);
-              done();
-            });
-          });
-        });
       }
     });
   }
