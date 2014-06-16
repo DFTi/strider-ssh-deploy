06d9645ac3c837e338a01aa54b6ecded2f080e88
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-06-16 10:56:24 -0700

    test tarball creation

--- test/bundler_test.js
+++ test/bundler_test.js
@@ -7,13 +7,21 @@ var bundler = require('../bundler');
 var fs = require('fs');
 
 describe("bundler", function() {
+  var err = null, res = null;
+
   describe("bundleProject", function() {
-    it("creates a tarball of the project", function(done) {
-      bundler.bundleProject(__dirname, 'test', sinon.stub(), function(err, res) {
-        expect(err).to.be.null;
-        expect(fs.existsSync(res)).to.be.true;
-        done();
+    before(function(done) {
+      err = new Error('foo');
+      bundler.bundleProject(__dirname, 'test', sinon.stub(), function(_err, _res) {
+        err = _err; res = _res; done();
       });
+    });
+    it("creates a tarball of the project", function() {
+      expect(err).to.be.null;
+      expect(fs.existsSync(res)).to.be.true;
+    });
+    after(function(done) {
+      if (!err) { fs.unlink(res, done) };
     });
   });
 });
